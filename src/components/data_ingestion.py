4620aa4ea95d38be9e7c24640f912d5647c1a2fc
l011yp0p <popmykawaiilolly@gmail.com>
2026-03-25 13:13:28 +0530

    model trained

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -19,6 +19,9 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataFormationConfig
 from src.components.data_transformation import DataTransformation
 
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+
 @dataclass
 class DataIngestionConfig:
     BASE_DIR=os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -58,4 +61,7 @@ if __name__ == "__main__":
     train_data, test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
